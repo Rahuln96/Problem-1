ce43d7bb0db29d3fbd4d8944a25b3b15e1c6a914
Rahul Kumar <26121248+Rahuln96@users.noreply.github.com>
2022-09-11 13:46:36 -0700

    Hospital website modified
    Hospita website project

---
+++ Modified-Template/README.md
@@ -0,0 +1,30 @@
+# Problem-1
+
+Template Name : MEDINOVA Hospital Website Template
+
+Modified
+
+Theme Color :-  Color Change
+
+All Page  breadcrumbs Add
+
+Hero Slider :-  Image Change
+
+Add 3 Box :- 24 hour service | Professional Staff | Emergency Care
+
+About Us Section :-  Button Adding
+
+Our Service Section :- Design
+
+Our Facilities Addding
+
+Book Appointemnt Adding
+
+About Us Page:- Adding Counter, Adding Call Button
+
+Service Page :- Design
+
+
+
+
+
---
+++ Modified-Template/js/main.js
@@ -0,0 +1,78 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: true,
+        loop: true,
+    });
+    
+})(jQuery);
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 20, 1000);
+    counter("count2", 0, 500, 1000);
+    counter("count3", 0, 1200, 1000);
+    counter("count4", 0, 1000, 1000);
+   });
+
